65be9509841e085dc5318f0bef02a871c7ea5f00
dustan1 <goadustan@gmail.com>
2014-10-02 21:22:21 -0400

    stuff! Also, please read the FirstTest.
    If you have time.

---
+++ Scripts/Dorian Stuff/FirstTest.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+//Dorian's stuff! Testing for testing.
+
+
+public class NonMobileEntity : MonoBehaviour {
+	bool isWall;
+
+	//How do I add a hitbox? 
+
+}
