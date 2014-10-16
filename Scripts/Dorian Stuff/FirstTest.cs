03ee6b90120a77af254a189e8f6462686f805f35
dustan1 <goadustan@gmail.com>
2014-10-15 22:19:00 -0400

    things? Maybe.

--- Scripts/Dorian Stuff/FirstTest.cs
+++ Scripts/Dorian Stuff/FirstTest.cs
@@ -4,9 +4,30 @@ using System.Collections;
 //Dorian's stuff! Testing for testing.
 
 
-public class NonMobileEntity : MonoBehaviour {
-	bool isWall;
+public class coverWall : MonoBehaviour {
+
+	public int height;
+	public int width;
+	public int depth;
 
 	//How do I add a hitbox? 
+	tracks[];// = new guideTrack[4];
+
+}
+
+
+
+
+
+
+public class guideTrack {
+	public int x1;
+	public int y1;
+
+	public int x2; public int y2;
+//This needs to be changed into vectors, but not now...
+	coverWall* parentObject;
+
+
 
 }
