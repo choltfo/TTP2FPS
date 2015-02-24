b7dfb37dadfbff8c26ffc678995e73bb2793cda4
Charles <charles.holtforster@gmail.com>
2015-02-23 22:02:24 -0500

    The new UI system is crazy cool. But it's a pain in the rear.

--- Scripts/Player/Pause.cs
+++ Scripts/Player/Pause.cs
@@ -5,12 +5,8 @@ public class Pause : MonoBehaviour {
 
 	public bool paused = false;
 	public bool pauseGameplay = true; // In multiplayer, this will be set to false so that the player keeps sending updates when 
-
-	// Use this for initialization
-	void Start () {
-
-	}
 	
+
 	// Update is called once per frame
 	void Update () {
 		Screen.lockCursor = !paused;
