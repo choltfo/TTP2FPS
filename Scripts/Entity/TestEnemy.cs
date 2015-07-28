a4ed384858b6e589de37d5cdbf22278d162d9cca
Charles <charles.holtforster@gmail.com>
2015-07-27 21:16:06 -0400

    Enemy animation attempts.
    It's a start.
    Needs documentation of animation triggers for future reference

---
+++ Scripts/Entity/TestEnemy.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestEnemy : CombatantEntity {
+	
+	public string[] animNames = {
+		"idle",
+		"idleCrouch",
+		"idleAiming",
+		"jump",
+		"melee",
+		"forward",
+		"forwardLeft",
+		"forwardRight",
+		"left",
+		"right",
+		"turnLeft",
+		"turnRight",
+		"backward",
+		"backwardLeft",
+		"backwardRight",
+		"dance"
+	};
+	
+	public float moveSpeedFwd;
+	public float moveSpeedBck;
+	public float moveSpeedStf;
+	
+	bool triggered = false;
+	
+	// Use this for initialization
+	void Start () {
+		//GetComponent<Animator>().SetTrigger("IDLE");
+		weapons[currentWeapon].enableWeapon(this);
+		//GetComponent<Animator>().StartPlayback();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Time.time > 5) GetComponent<Animator>().Play (animNames[(int)Anims.idleAiming]);
+		if (Time.time > 5 && !triggered) {
+			GetComponent<Animator>().SetTrigger("IDLE_AIMING");
+			triggered = true;
+			print ("Triggering IDLE_AIMING");
+		}
+	}
+}
