3d3aac2124e5f06f873271e4e1e2963cfb2be977
Charles <charles.holtforster@gmail.com>
2015-07-29 15:29:33 -0400

    Switched out the animation triggerer
    Some of my finest ternary operators!

--- Scripts/Entity/TestEnemy.cs
+++ Scripts/Entity/TestEnemy.cs
@@ -26,57 +26,88 @@ public class TestEnemy : CombatantEntity {
 	public float moveSpeedBck;
 	public float moveSpeedStf;
 	
+	public Animator anim;
 	public NavMeshAgent navMeshAgent;
 	public Vector3 currentTarget;
 	Vector3 lastNMDirection;
 	
 	bool isIdle;
 	
+	const float sin22p5 = 0.382683;
 	
 	// Use this for initialization
 	void Start () {
-		//GetComponent<Animator>().SetTrigger("IDLE");
+		//anim.SetTrigger("IDLE");
 		weapons[currentWeapon].enableWeapon(this);
-		//GetComponent<Animator>().StartPlayback();
+		//anim.StartPlayback();
 	}
 	
-	// Update is called once per frame
+	/*
+		NOTE:
+		Animated sub-object should always face (anim.transform.forward) towards the enemy.
+		Movement is handled exclusively by the NavMeshAgent, while animation is handled by this script and
+			the sub-object, which is the root of the gun. This way, it will backpedal while firing when applicable.
+	*/
 	void Update () {
 		navMeshAgent.SetDestination(currentTarget);
 		
-		Vector3 localNMDirection = transform.InverseTransformDirection(navMeshAgent.desiredVelocity);
+		Vector3 animLocalDirection = anim.transform.InverseTransformDirection(navMeshAgent.desiredVelocity).normalized;
 		
-		print (localNMDirection);
+		print (animLocalDirection);
 		
-		if ((localNMDirection - lastNMDirection).sqrMagnitude > 0.2) { 
-			if (localNMDirection.x > 0) {
-				GetComponent<Animator>().SetTrigger("MOVE_FORWARD_RIGHT");
-				print ("Turning Right");
-			} else if (localNMDirection.x < 0) {
-				GetComponent<Animator>().SetTrigger("MOVE_FORWARD_LEFT");
-				print ("Turning Left");
+		anim.transform.forward = Vector3.forward;	// NOTE: Change this when targetting is a thing! Should point to either direction of travel or target.
+		
+		Debug.DrawLine(transform.position, transform.position+navMeshAgent.desiredVelocity, Color.cyan);
+		Debug.DrawLine(transform.position, transform.position+animLocalDirection, Color.red);
+		
+		int XTravel = 0;
+		int ZTravel = 0;
+		//sin22p5
+		if (Mathf.Abs(animLocalDirection.x) > sin22p5) XTravel = animLocalDirection.x/Mathf.Abs(animLocalDirection.x);
+		if (Mathf.Abs(animLocalDirection.z) > sin22p5) ZTravel = animLocalDirection.z/Mathf.Abs(animLocalDirection.z);
+		
+		if ((animLocalDirection - lastNMDirection).sqrMagnitude > 0.0001f) { 
+			anim.SetTrigger("MOVE_" + (ZTravel == 0 ? "" : (ZTravel > 0 ? "FORWARD" : "BACKWARD")) + (XTravel == 0 ? "" : (XTravel > 0 ? "_RIGHT" : "_LEFT")));
+			
+			/*if (animLocalDirection.z > 0) {
+				if (animLocalDirection.x > 0) {
+					anim.SetTrigger("MOVE_FORWARD_RIGHT");
+					print ("Turning Right");
+				} else if (animLocalDirection.x < 0) {
+					anim.SetTrigger("MOVE_FORWARD_LEFT");
+					print ("Turning Left");
+				} else {
+					anim.SetTrigger("MOVE_FORWARD");
+					print ("Moving Forward");
+				}
 			}
-			if (localNMDirection.z > 0) {
-				GetComponent<Animator>().SetTrigger("MOVE_FORWARD");
-				print ("Moving Forward");
-			} else if (localNMDirection.z < 0) {
-				GetComponent<Animator>().SetTrigger("MOVE_BACKWARD");
-				print ("Moving Backward");
-			}
+			if (animLocalDirection.z < 0) {
+				if (animLocalDirection.x > 0) {
+					anim.SetTrigger("MOVE_BACKWARD_RIGHT");
+					print ("Turning Right");
+				} else if (animLocalDirection.x < 0) {
+					anim.SetTrigger("MOVE_BACKWARD_LEFT");
+					print ("Turning Left");
+				} else {
+					anim.SetTrigger("MOVE_BACKWARD");
+					print ("Moving Backward");
+				}
+			}*/
 			isIdle = false;
 		}
-		if (localNMDirection.sqrMagnitude < 0.1) {
-			if (!isIdle) GetComponent<Animator>().SetTrigger("IDLE_AIMING");
+		
+		if (animLocalDirection.sqrMagnitude < 0.1) {
+			if (!isIdle) anim.SetTrigger("IDLE_AIMING");
 			weapons[currentWeapon].trigger(this);
 			isIdle = true;
 			print ("Idling");
 		}
-		lastNMDirection = localNMDirection;
+		lastNMDirection = animLocalDirection;
 		
 		
-		//if (Time.time > 5) GetComponent<Animator>().Play (animNames[(int)Anims.idleAiming]);
+		//if (Time.time > 5) anim.Play (animNames[(int)Anims.idleAiming]);
 		/*if (Time.time > 5 && !triggered) {
-			GetComponent<Animator>().SetTrigger("IDLE_AIMING");
+			anim.SetTrigger("IDLE_AIMING");
 			triggered = true;
 			print ("Triggering IDLE_AIMING");
 		}*/
